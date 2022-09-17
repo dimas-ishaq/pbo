73ad9925a4743c55c4730843f38ead85d872078a
Dimas Maulana <67549847+MasJamad@users.noreply.github.com>
2022-09-17 17:15:34 +0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -6,8 +6,23 @@ public class Main {
         hewan b = new hewan();
         Manusia c = new Manusia();
 
-        c.mencuci("Proses mencuci pakaian");
-        b.bermain("Makan");
+        System.out.println("Manusia: ");
+        System.out.println("Bermain: " + c.bermain("CSO, POINT BLANK"));
+        System.out.println("Makan: " + c.makan("Gado - Gado"));
+        c.tidur(true);
+        System.out.println("Bekerja: " + c.bekerja("Karyawan"));
+        System.out.println("Memasak: " + c.memasak("Makanan"));
+        System.out.println("Mencuci: " + c.mencuci("Pakaian"));
+
+        System.out.println("Hewan: ");
+        System.out.println("Bermain: " + b.bermain("Lari - Lari"));
+        System.out.println("Makan: " + b.makan("Daging"));
+        b.tidur(true);
+
+        System.out.println("Telepon: ");
+        System.out.println("Menerima Panggilan: " + a.terimaPanggilan("Halo"));
+        System.out.println("Melakukan Panggilan: " + a.melakukan_Panggilan("Hallo"));
+
     }
 
 }
